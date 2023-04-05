8e18dbb80f81acf0e2a788df0f5482a3b1c8c839
LoBASTIEN <129037717+LoBASTIEN@users.noreply.github.com>
2023-04-05 17:11:02 +0200

    Add files via upload

--- Matlab/TEST_In_LB.m
+++ Matlab/TEST_In_LB.m
@@ -2,23 +2,44 @@ clc
 close all;
 clear all;
 
-% Seuillage auto (binarisation)
-C=imread('In_12.png');
+for i=10:50
 
-I=imread('In_12.bmp');
-I =imcomplement(I);
+    I= imread("C:\Users\loic\OneDrive\Documents\Analyse d'image\Projet Image\Source Images - bmp\In_"+ i+".bmp"); 
+    C= imread("C:\Users\loic\OneDrive\Documents\Analyse d'image\Projet Image\Ground truth - png\In_"+ i+".png"); 
 
-se = strel('disk',10);
-tophat = imtophat(I,se);
+    % Traitement de l'image
+    
+    I = imcomplement(I);
 
+    se = strel('disk',10);
+    tophat = medfilt2(I,[2 2]);
+    tophat = imtophat(BW,se);
+    
+    sf = strel('disk',1);
+   
+    BW = im2bw(tophat,graythresh(tophat));
+    
+    BW = imerode(BW, sf);
+    %BW = imdilate(BW, sf);
+    %BW = imclose(BW,sf);
+    
+    % Traitement de l'image_finale
 
-BW=im2bw(tophat,graythresh(tophat));
-BW =medfilt2(BW,[4 4]);
-sf = strel('disk',1);
-BW = imerode(BW, sf);
-BW = imdilate(BW, sf);
+    Igt = imbinarize(C,graythresh(C)); 
 
-subplot(2,2,1), imshow(I), title('Image originale');
-subplot(2,2,2), imshow(tophat), title('Image top-hat');
-subplot(2,2,3), imshow(BW), title('Image finale');
-subplot(2,2,4), imshow(C), title('Comparaison');
+    % Différence
+
+    Idiff1 = Igt - BW; 
+    Idiff2 = BW -Igt; 
+    If = max(Idiff1,Idiff2); 
+
+    % Afficher l'image originale, l'image top-hat, l'image finale et la comparaison
+    subplot(2,3,1), imshow(I), title('Image originale');
+    subplot(2,3,2), imshow(tophat), title('Image top-hat');
+    subplot(2,3,3), imshow(BW), title('Image finale');
+    subplot(2,3,4), imshow(C), title('Image prof');
+    subplot(2,3,5), imshow(If), title('Différence');
+
+    
+    pause(0.5); % Pause pour éviter de surcharger le processeur
+end
